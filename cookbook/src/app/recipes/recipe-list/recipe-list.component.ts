5b05ce26c86a31185072a308b79674d5ac7643d9
ewerton.marschalk <ewerton.marschalk@redeunifique.com.br>
2022-04-26 14:16:50 -0300

    components databidings

--- cookbook/src/app/recipes/recipe-list/recipe-list.component.ts
+++ cookbook/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -8,12 +8,20 @@ import { Recipe } from '../recipe.model';
 })
 export class RecipeListComponent implements OnInit {
   recipes: Recipe[] = [
-    new Recipe('A test recipe', 'this is a description', 'https://ps.w.org/image-hover-effects-block/assets/icon-128x128.png')
+    new Recipe('A test recipe', 'this is a description', 'https://ps.w.org/image-hover-effects-block/assets/icon-128x128.png'),
+    new Recipe('Another recipe', 'this is another description', 'https://ps.w.org/image-hover-effects-block/assets/icon-128x128.png')
   ];
+
+  @Output()
+  recipeWasSelected = new EventEmitter<Recipe>();
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onRecipeSelected(recipe: Recipe){
+    this.recipeWasSelected.emit(recipe);
   }
 
 }
